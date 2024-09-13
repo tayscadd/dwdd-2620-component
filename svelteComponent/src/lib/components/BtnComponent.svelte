29577a7881b68865326d3fddcc9f9f423c6ff2ff
tayscadd <taylor@scaddenfamily.com>
2024-09-12 19:58:31 -0600

    Svelte Version of the buttons

---
+++ svelteComponent/src/lib/components/BtnComponent.svelte
@@ -0,0 +1,34 @@
+<script>
+    export let txt = 'Button' // Defaults to Button
+    export let size = 'md'; // sm, md, lg
+    export let disabled = false; // true, false
+
+    
+    let cls = ['flex', 'px-4', 'w-fit', 'flex-row', 'gap-2', 'border-none','place-items-center', 'justify-center', 'h-11',`font-['Inter']`, 'border-none', 'bg-[#6B4EFF]', 'text-white', 'rounded-[999999px]', 'hover:bg-[#5538EE]', 'disabled:bg-[#E3E5E5]', 'disabled:text-[#979C9E]', 'group']
+    
+    if (size === 'sm') {cls.push('min-w-24')}
+    else if (size === 'md') {cls.push('min-w-36')}
+    else if (size === 'lg') {cls.push('min-w-80')}
+
+    let class_string = cls.join(' ')
+</script>
+
+<style></style>
+
+{#if disabled == false}
+    <button class='{class_string}'>
+        <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg">
+            <circle class="group-disabled:stroke-[#979C9E] origin-center" cx="8" cy="8" r="7" fill="none" stroke="white" stroke-width="1"/>
+        </svg>
+
+        <span>{txt}</span>
+    </button>
+{:else}
+    <button class='{class_string}' disabled>
+        <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg">
+            <circle class="group-disabled:stroke-[#979C9E] origin-center" cx="8" cy="8" r="7" fill="none" stroke="white" stroke-width="1"/>
+        </svg>
+
+        <span>{txt}</span>
+    </button>
+{/if}
---
+++ svelteComponent/src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+    import BtnComponent from '$lib/components/BtnComponent.svelte'
+</script>
+
+<style>
+    body {
+        min-width: 100%;
+        min-height: 100vh;
+        display: grid;
+        place-items: center;
+    }
+    div {
+        display: flex;
+        flex-direction: column;
+        gap: 8px;
+    }
+</style>
+
+<body>
+    <div>
+        <BtnComponent size='lg' />
+        <BtnComponent size='md' />
+        <BtnComponent size='sm' disabled=true/>
+    </div>
+</body>
